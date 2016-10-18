c3f5ef243ee43bb7191501c8d406c2ba877d02ad
han361769045 <Qiang2Han513>
2016-10-18 18:43:14 +0800

    upload bintray

---
+++ app/src/main/java/com/lu/leo/llrecyclerview/demo/MainActivity.java
@@ -0,0 +1,10 @@
+package com.lu.leo.llrecyclerview.demo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LeoLu on 2016/10/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
